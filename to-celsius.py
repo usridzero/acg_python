085e2f1f1d5ec56fa98eb72e3c23a7e12855a25e
usridzero <usridzero@gmail.com>
2021-08-25 00:34:33 +0000

    modify first lab.

--- to-celsius.py
+++ to-celsius.py
@@ -1,6 +1,6 @@
 #!/usr/local/bin/python3.7
 
 temp = float(input("What temperature (in Fahrenheit) would you like to convert to Celsius?: "))
-celsius = (temp - 32) * 5.0/9.0
+celsius = (temp - 32) * 5 /9
 
-print(temp, "F is", celsius, "C")
+print(temp, "F is", round(celsius, 2), "C")
